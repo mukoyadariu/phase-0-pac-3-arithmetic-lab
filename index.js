90673a69e777819012731a495e24a52f23ef0753
mukoyadarius <mukoya.darius@student.moringaschool.com>
2023-07-20 19:43:21 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b){
+    let result =  a + b;
+    return result
+}
+function subtract(a,b){
+    let result = a - b;
+    return result
+}
+function multiply(a,b){
+    let result = a * b
+    return result
+}
+function divide(a,b){
+    let result = a / b
+    return result
+}
+function increment(n){
+    return n + 1;
+}
+function decrement(n){
+    return n -1;
+}
+function makeInt(n){
+   return  parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
